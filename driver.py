2a7edff173d3255bee7f3bfd5e20f1cd28e13917
Joseph Downs <Joseph.Downs@jhuapl.edu>
2017-08-22 20:36:42 -0400

    refactor: add prints to driver and remove french

--- driver.py
+++ driver.py
@@ -7,29 +7,26 @@ from score import *
 
 if __name__ == '__main__':
     # load ud data
+    print('Loading tagged data...')
     ud_en_wd, ud_en_pos = ud_load('en')
     ud_fi_wd, fi_true_pos = ud_load('fi')
-    ud_fr_wd, fr_true_pos = ud_load('fr')
+    print('Tagged data loaded...')
     # load ep data
+    print('Loading parallel data...')
     ep_fien_en_wd, ep_fien_fi_wd = ep_load('en', 'fi')
-    ep_fren_en_wd, ep_fren_fr_wd = ep_load('en', 'fr')
+    print('Parallel data loaded...')
     # run tagger
-    tagger = noun_tagger
+    tagger = cluster_tagger
+    print('Running tagger...')
     fi_pred_pos = tagger(
             ud_en_wd,
             ud_en_pos,
             ep_fien_fi_wd,
             ep_fien_en_wd,
             ud_fi_wd)
-    fr_pred_pos = tagger(
-            ud_en_wd,
-            ud_en_pos,
-            ep_fren_fr_wd,
-            ep_fren_en_wd,
-            ud_fr_wd)
+    print('Tagger complete...')
     # score tags
+    print('Scoring tags...')
     fi_score = score(fi_pred_pos, fi_true_pos)
-    fr_score = score(fr_pred_pos, fr_true_pos)
     print('Finnish score: {}'.format(fi_score))
-    print('French score: {}'.format(fr_score))
 
