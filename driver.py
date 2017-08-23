0b1c44bc1dcebafd2b6b0ea8ffc1a79ac64f5dee
Joseph Downs <Joseph.Downs@jhuapl.edu>
2017-08-22 21:59:39 -0400

    feat: make driver run over multiple taggers

--- driver.py
+++ driver.py
@@ -15,18 +15,20 @@ if __name__ == '__main__':
     print('Loading parallel data...')
     ep_fien_en_wd, ep_fien_fi_wd = ep_load('en', 'fi')
     print('Parallel data loaded...')
-    # run tagger
-    tagger = cluster_tagger
-    print('Running tagger...')
-    fi_pred_pos = tagger(
-            ud_en_wd,
-            ud_en_pos,
-            ep_fien_fi_wd,
-            ep_fien_en_wd,
-            ud_fi_wd)
-    print('Tagger complete...')
-    # score tags
-    print('Scoring tags...')
-    fi_score = score(fi_pred_pos, fi_true_pos)
-    print('Finnish score: {}'.format(fi_score))
+    # run taggers
+    for tagger in [noun_tagger, cluster_tagger]:
+        print('Running tagger...')
+        fi_pred_pos = tagger(
+                ud_en_wd,
+                ud_en_pos,
+                ep_fien_fi_wd,
+                ep_fien_en_wd,
+                ud_fi_wd)
+        print('Tagger complete...')
+        # score tags
+        print('Scoring tags...')
+        fi_score = score(fi_pred_pos, fi_true_pos)
+        print('{} score on Finnish: {}'.format(
+            tagger.__name__,
+            fi_score))
 
